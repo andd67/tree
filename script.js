a60ea9a4e699b76c23fae1a2914bc6d7340c5a8a
Андрей <itc-d-aa@yandex.ru>
2019-01-20 18:39:10 +0300

    Начал вести историю

---
+++ script.js
@@ -0,0 +1,9 @@
+   var container = document.querySelector('.container');
+   var initialElement = document.createElement('div');
+   initialElement.classList.add('raz');
+   initialElement.textContent = '1000 рублей';
+   container.appendChild(initialElement);
+
+
+   
+   
